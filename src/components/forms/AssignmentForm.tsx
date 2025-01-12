f9eba8d95ee880914834fed4dd8ad5bf3b3aca5e
Ernest Aboah <ernesco28@gmail.com>
2025-01-12 10:55:37 +0000

    updated the assignment form fields to include notes and file uploads to firebase storage

--- src/components/forms/AssignmentForm.tsx
+++ src/components/forms/AssignmentForm.tsx
@@ -4,35 +4,104 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import InputField from "../InputField";
-import Image from "next/image";
-
-const schema = z.object({
-  subjectName: z.string().min(1, { message: "Subject Name is required!" }),
-  class: z.string().min(1, { message: "Class is required!" }),
-  teacher: z.string().min(1, { message: "Teacher is required!" }),
-  date: z.date({ message: "Date is required!" }),
-});
-
-type Inputs = z.infer<typeof schema>;
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { assignmentSchema, AssignmentSchema } from "@/lib/formValidationSchema";
+import { useFormState } from "react-dom";
+import { createAssignment, updateAssignment } from "@/lib/action";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+import FileUpload from "../FileUpload";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { storage } from "../../lib/firebaseConfig";
 
 const AssignmentForm = ({
   type,
   data,
+  setOpen,
+  relatedData,
 }: {
   type: "create" | "update";
   data?: any;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  relatedData?: any;
 }) => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<Inputs>({
-    resolver: zodResolver(schema),
+  } = useForm<AssignmentSchema>({
+    resolver: zodResolver(assignmentSchema),
   });
 
-  const onSubmit = handleSubmit((data) => {
-    console.log(data);
-  });
+  const [file, setFile] = useState<File | null>(null);
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+  const [downloadURL, setDownloadURL] = useState<string | null>(null);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+    }
+  };
+
+  const handleUpload = () => {
+    if (!file) return;
+
+    const storageRef = ref(storage, `documents/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadProgress(progress);
+      },
+      (error) => {
+        console.error("Upload error:", error);
+      },
+      async () => {
+        const url = await getDownloadURL(uploadTask.snapshot.ref);
+        setDownloadURL(url);
+        console.log("File available at", url);
+      },
+    );
+  };
+
+  const [state, formAction] = useFormState(
+    type == "create" ? createAssignment : updateAssignment,
+    {
+      success: false,
+      error: false,
+    },
+  );
+
+  const onSubmit = handleSubmit(
+    (data) => {
+      const completeData = { ...data, file: downloadURL };
+      console.log(completeData);
+      //formAction(data);
+    },
+    (errors) => {
+      Object.values(errors).forEach((error) => {
+        toast.error(error.message);
+      });
+    },
+  );
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (state.success) {
+      toast(
+        `Assignment ${type === "create" ? "Created" : "updated"} successfully`,
+      );
+      setOpen(false);
+      router.refresh();
+    }
+  }, [state, router, type, setOpen]);
+
+  const { assignmentSubjects, assignmentClass, assignmentTeacher } =
+    relatedData;
 
   return (
     <form className="flex flex-col gap-8" onSubmit={onSubmit}>
@@ -44,34 +113,96 @@ const AssignmentForm = ({
         Assignment Information
       </span>
       <div className="flex justify-between gap-4 flex-wrap">
-        <InputField
-          label="Subject Name"
-          name="subjectName"
-          defaultValue={data?.subjectName}
-          register={register}
-          error={errors?.subjectName}
-        />
-        <InputField
-          label="Class"
-          name="class"
-          defaultValue={data?.class}
-          register={register}
-          error={errors?.class}
-        />
-        <InputField
-          label="Teacher"
-          name="teacher"
-          defaultValue={data?.teacher}
-          register={register}
-          error={errors?.teacher}
-        />
+        <div className="w-full flex justify-between">
+          <div className="flex flex-col gap-2 w-full md:w-1/4">
+            <label className="text-lg text-gray-500">Subject Title</label>
+            <select
+              className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+              {...register("subjectId")}
+              defaultValue={data?.subjectId}
+            >
+              {assignmentSubjects.map(
+                (lesson: { id: number; name: string }) => {
+                  return (
+                    <option key={lesson.id} value={lesson.id}>
+                      {lesson.name}
+                    </option>
+                  );
+                },
+              )}
+            </select>
+          </div>
+          <div className="flex flex-col gap-2 w-full md:w-1/4">
+            <label className="text-lg text-gray-500">Class</label>
+            <select
+              className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+              {...register("classId")}
+              defaultValue={data?.classId}
+            >
+              {assignmentClass.map((classes: { id: number; name: string }) => {
+                return (
+                  <option key={classes.id} value={classes.id}>
+                    {classes.name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="flex flex-col gap-2 w-full md:w-1/4">
+            <label className="text-lg text-gray-500">Teacher</label>
+            <select
+              className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+              {...register("teacherId")}
+              defaultValue={data?.teacherId}
+            >
+              {assignmentTeacher.map(
+                (teacher: {
+                  id: string;
+                  firstName: string;
+                  lastName: string;
+                }) => {
+                  return (
+                    <option key={teacher.id} value={teacher.id}>
+                      {`${teacher.firstName} ${teacher.lastName}`}
+                    </option>
+                  );
+                },
+              )}
+            </select>
+          </div>
+        </div>
+        <div className="w-full flex flex-wrap gap-6  mb-6">
+          <label className="text-lg text-gray-500">Notes</label>
+          <textarea
+            defaultValue={data?.notes}
+            {...register("notes")}
+            className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-lg w-full"
+          ></textarea>
+        </div>
+
+        {/* <FileUpload /> */}
+        <div className="flex flex-col gap-2 w-full md:w-2/5">
+          <input
+            type="file"
+            onChange={handleFileChange}
+            // {...register("file")}
+          />
+          <button type="button" onClick={handleUpload}>
+            Upload
+          </button>
+          {uploadProgress > 0 && <p>Upload Progress: {uploadProgress}%</p>}
+          {downloadURL && (
+            <p>
+              File uploaded successfully! <a href={downloadURL}>View File</a>
+            </p>
+          )}
+        </div>
 
         <InputField
           label="Due Date"
           name="date"
           defaultValue={data?.date}
           register={register}
-          error={errors?.date}
           type="date"
         />
       </div>
